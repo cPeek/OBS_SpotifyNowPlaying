d7d9c42e6f41381531b7b37d45469c57fa2dbde2
cPeek <cameron.peek15@gmail.com>
2017-11-05 03:09:49 -0600

    ye

--- OBSSpotifyNowPlaying_Forms/Form1.Designer.cs
+++ OBSSpotifyNowPlaying_Forms/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace OBSSpotifyNowPlaying_Forms {
-	partial class Form1 {
+	partial class OBSSpotifyNowPlaying {
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -23,12 +23,80 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OBSSpotifyNowPlaying));
+			this.text_file = new System.Windows.Forms.TextBox();
+			this.btn_browse = new System.Windows.Forms.Button();
+			this.text_output = new System.Windows.Forms.TextBox();
+			this.btn_start = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// text_file
+			// 
+			this.text_file.Location = new System.Drawing.Point(12, 12);
+			this.text_file.Name = "text_file";
+			this.text_file.Size = new System.Drawing.Size(260, 20);
+			this.text_file.TabIndex = 0;
+			this.text_file.TextChanged += new System.EventHandler(this.text_file_TextChanged);
+			// 
+			// btn_browse
+			// 
+			this.btn_browse.Location = new System.Drawing.Point(12, 38);
+			this.btn_browse.Name = "btn_browse";
+			this.btn_browse.Size = new System.Drawing.Size(75, 23);
+			this.btn_browse.TabIndex = 1;
+			this.btn_browse.Text = "Browse";
+			this.btn_browse.UseVisualStyleBackColor = true;
+			this.btn_browse.Click += new System.EventHandler(this.btn_browse_Click);
+			// 
+			// text_output
+			// 
+			this.text_output.Location = new System.Drawing.Point(12, 89);
+			this.text_output.Multiline = true;
+			this.text_output.Name = "text_output";
+			this.text_output.ReadOnly = true;
+			this.text_output.Size = new System.Drawing.Size(260, 59);
+			this.text_output.TabIndex = 2;
+			// 
+			// btn_start
+			// 
+			this.btn_start.Location = new System.Drawing.Point(197, 38);
+			this.btn_start.Name = "btn_start";
+			this.btn_start.Size = new System.Drawing.Size(75, 23);
+			this.btn_start.TabIndex = 3;
+			this.btn_start.Text = "Start";
+			this.btn_start.UseVisualStyleBackColor = true;
+			this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "now-playing.txt";
+			// 
+			// OBSSpotifyNowPlaying
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(284, 158);
+			this.Controls.Add(this.btn_start);
+			this.Controls.Add(this.text_output);
+			this.Controls.Add(this.btn_browse);
+			this.Controls.Add(this.text_file);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "OBSSpotifyNowPlaying";
+			this.Text = "Spotify Now Playing";
+			this.Load += new System.EventHandler(this.OBSSpotifyNowPlaying_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox text_file;
+		private System.Windows.Forms.Button btn_browse;
+		private System.Windows.Forms.TextBox text_output;
+		private System.Windows.Forms.Button btn_start;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 	}
 }
 
--- OBSSpotifyNowPlaying_Forms/Form1.cs
+++ OBSSpotifyNowPlaying_Forms/Form1.cs
@@ -8,10 +8,130 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+using System.IO;
+
 namespace OBSSpotifyNowPlaying_Forms {
-	public partial class Form1 : Form {
-		public Form1() {
+	public partial class OBSSpotifyNowPlaying : Form {
+
+		private SpotifyLocalAPI spotify;
+		private string filePath;
+
+		public OBSSpotifyNowPlaying() {
 			InitializeComponent();
+		}
+
+		private void OBSSpotifyNowPlaying_Load(object sender, EventArgs e) {
+			spotify = new SpotifyLocalAPI();
+			text_output.Text = "OBS Spotify Now Playing";
+		}
+
+		private void text_file_TextChanged(object sender, EventArgs e) {
+			filePath = text_file.Text;
+		}
+
+		private void btn_start_Click(object sender, EventArgs e) {
+			bool webHelperRunning = SpotifyLocalAPI.IsSpotifyWebHelperRunning();
+			bool spotifyRunning = SpotifyLocalAPI.IsSpotifyRunning();
+
+			if(spotifyRunning == true) {
+				outputText("Spotify is running.");
+			} else {
+				outputText("Spotify is not running. Starting Spotify.");
+				SpotifyLocalAPI.RunSpotify();
+			}
+
+			if(webHelperRunning == true) {
+				outputText("WebHelper is running");
+			} else {
+				outputText("WebHelper is not running. Starting WebHelper.");
+				SpotifyLocalAPI.RunSpotifyWebHelper();
+			}
+
+			if (spotify.Connect()) {
+				outputText("Connected to Spotify.");
+			} else {
+				outputText("Couldn't connect to Spotify.");
+			}
+
+			spotify.ListenForEvents = true;
+			spotify.OnTrackChange += trackChanged;
+			spotify.OnPlayStateChange += playStateChanged;
+
+			clearText();
+
+			/*StatusResponse sr = spotify.GetStatus();
+			if(sr.Playing == true) {
+				Track tr = sr.Track;
+				string trackName = tr.TrackResource.Name;
+				string artistName = tr.ArtistResource.Name;
+
+				clearText();
+				outputText(trackName);
+				outputText(artistName);
+				writeFile(trackName + "\n" + artistName);
+			} else {
+				clearText();
+				outputText("No Track");
+				outputText("N/A");
+				writeFile("No Track \n N/A");
+			}*/
+
+		}
+
+		private void playStateChanged(object sender, PlayStateEventArgs e) {
+			clearText();
+			outputText("No Track");
+			outputText("N/A");
+
+			writeFile("No Track \n N/A");
+		}
+
+		private void trackChanged(object sender, TrackChangeEventArgs e) {
+			Track track = e.NewTrack;
+			string trackName = track.TrackResource.Name;
+			string artistName = track.ArtistResource.Name;
+
+			clearText();
+			outputText(trackName);
+			outputText(artistName);
+
+			writeFile(trackName + "\n" + artistName);
+		}
+
+		private void btn_browse_Click(object sender, EventArgs e) {
+			if(openFileDialog1.ShowDialog() == DialogResult.OK) {
+				filePath = openFileDialog1.FileName;
+				text_file.Text = filePath;
+			}
+		}
+
+		private void outputText(string output) {
+			if (InvokeRequired) {
+				this.Invoke(new Action<string>(outputText), new object[] { output });
+				return;
+			}
+			text_output.Text+= "\r\n" + output;
+		}
+		private void clearText() {
+			if (InvokeRequired) {
+				this.Invoke(new Action(clearText));
+				return;
+			}
+			text_output.Clear();
+		}
+
+		private void writeFile(string text) {
+			try {
+				StreamWriter sw = new StreamWriter(filePath);
+				sw.Write(text);
+				sw.Close();
+			} catch(Exception ex) {
+				clearText();
+				outputText(ex.Message);
+			}
 		}
 	}
 }
